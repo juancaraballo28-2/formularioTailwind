3a4f629591b96c1b349253f2960ec7e130f1376f
Juan Felipe Caraballo <69644034+juancaraballo28-2@users.noreply.github.com>
2022-07-05 11:02:14 -0500

    Version final sin comentarios

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
-    $('.slider-caracteristicas').slick({
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      centerMode: true,
-      variableWidth: true
-    });
+  $('.slider-caracteristicas').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
   });
+});
