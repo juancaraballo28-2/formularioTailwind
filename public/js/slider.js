f89444595d4d5135fac45c115e30052560362ae8
Juan Felipe Caraballo <69644034+juancaraballo28-2@users.noreply.github.com>
2022-07-05 10:49:32 -0500

    Version final con comentarios

---
+++ public/js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.slider-caracteristicas').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: true
+    });
+  });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing:{
+        '104': '28rem',
+        '112': '34rem'
+      }
+    },
   },
   plugins: [],
 }
